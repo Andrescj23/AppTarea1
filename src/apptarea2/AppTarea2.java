c8220844b40765a349adc1ea2ca09976f8c6763a
Andres Chaj <andreschaj98@gmail.com>
2018-06-07 16:02:57 -0600

    acualizacion andres

--- src/apptarea2/AppTarea2.java
+++ src/apptarea2/AppTarea2.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package apptarea2;
 
 import javafx.application.Application;
@@ -12,40 +8,67 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import com.apptarea2.Controller.LoginController;
+import java.io.IOException;
+import java.io.InputStream;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.layout.AnchorPane;
 
-/**
- *
- * @author desarrollo
- */
+
 public class AppTarea2 extends Application {
+           
+   private Stage escenarioPrincipal;
+   private final String PaqueteVista = "/com/apptarea2/View/";
+   @Override
+   
+   public void start(Stage escenarioPrincipal){
+       
+       this.escenarioPrincipal = escenarioPrincipal;
+       this.escenarioPrincipal.setTitle("Tarea 1.0");
+       
+       mostrarLogin();
+       this.escenarioPrincipal.show();
+       
+   }
+   
+   public void mostrarLogin(){
+       
+       try {
+           LoginController login =(LoginController)cambiarEscena("LoginView.fxml",600,400);
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       
+   }
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
+   public Initializable cambiarEscena(String fxml, int ancho, int alto) throws IOException{
+       
+       Initializable resultado = null;
+       
+       FXMLLoader cargadorFXML = new FXMLLoader();
+       
+       InputStream archivo = AppTarea2.class.getResourceAsStream(PaqueteVista+fxml);
+       
+       cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
+       
+       cargadorFXML.setLocation(AppTarea2.class.getResource(PaqueteVista+fxml));
+       
+       Scene escena = new Scene ((AnchorPane)cargadorFXML.load(archivo), ancho,alto);
+       
+       this.escenarioPrincipal.setScene(escena);
+       
+       this.escenarioPrincipal.sizeToScene();
+       
+       resultado = (Initializable)cargadorFXML.getController();
+       return resultado;
+       
+       
+   }
+   
+   public static void main(String args[]){
+       
+       launch(args);
+   }
 }
